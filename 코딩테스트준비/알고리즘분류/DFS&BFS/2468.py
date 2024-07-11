bd5991374bb687907f3f81b8712f959b1d0b10c6
정재욱 <wjdwodnr5452@naver.com>
2024-07-11 23:37:31 +0900

    2468

--- 코딩테스트준비/알고리즘분류/DFS&BFS/2468.py
+++ 코딩테스트준비/알고리즘분류/DFS&BFS/2468.py
@@ -1,30 +1,38 @@
 import sys
+sys.setrecursionlimit(100000)
 
 n = int(sys.stdin.readline())
 
+# 상하좌우
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+
 graph = []
 
-check_graph = [[False for _ in range(n)] for _ in range(n)]
 
-max_num = 0
-
-result = 0
-
-def dfs(num, v) :
-    print()
-
-
+def dfs(h, x, y) :
+    for m in range(4):
+        nx = x + dx[m]
+        ny = y + dy[m]
+        if(0 <= nx < n) and (0 <= ny < n) and not check_graph[nx][ny] and graph[nx][ny] > h :
+            check_graph[nx][ny] = True
+            dfs(h, nx, ny)
 
 for i in range(n) :
     m = list(map(int, sys.stdin.readline().split()))
-    if(max(m) > max_num) :
-        max_num = max(m)
     graph.append(m)
 
-for i in range(max_num) :
-    for j in range(n) :
-        dfs_num = dfs(i, j)
-        if(dfs_num > result) :
-            result = dfs_num
+result = 1
 
-print(graph)
+for i in range(max(map(max, graph))) :
+    check_graph = [[False for _ in range(n)] for _ in range(n)]
+    count  =  0
+    for j in range(n) :
+        for k in range(n):
+            if graph[j][k] > i and not check_graph[j][k] :
+                count += 1
+                check_graph[j][k] = True
+                dfs(i, j, k)
+    result = max(result, count)
+       
+print(result)

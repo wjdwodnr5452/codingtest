f32ef03bf2b7623d19acfbe5940daccdefc864cf
정재욱 <wjdwodnr5452@naver.com>
2024-08-01 23:35:34 +0900

    3020

--- 코딩테스트준비/알고리즘분류/이진탐색/3020.py
+++ 코딩테스트준비/알고리즘분류/이진탐색/3020.py
@@ -1,6 +1,16 @@
 import sys
 
-def binary_search
+def binary_search(data_list, target) :
+    left = 0
+    right = len(data_list) - 1  #2
+    while left <= right :
+          mid = (left + right) // 2
+          if data_list[mid] <= target :
+               left = mid + 1
+          else :
+               right = mid - 1
+    print(right)
+    return len(data_list) - (right + 1)
 
 n, h = map(int, sys.stdin.readline().split())
 
@@ -16,7 +26,22 @@ for i in range(n) :
 bottom_arr.sort()
 top_arr.sort()
 
-result_arr = []
+ans = n
+cnt = 0
 
-for i in range(n) :
-     print()
+for i in range(1, h+1) :
+     down_num = binary_search(bottom_arr, i-1)
+     if(i == 1) :
+          print("down : " , down_num)
+     up_num = binary_search(top_arr, h-i)
+     cur_num = down_num + up_num
+     if cur_num < ans :
+          ans = cur_num
+          cnt = 1
+     elif cur_num == ans:
+          cnt += 1
+
+print(ans, cnt)
+
+
+     
